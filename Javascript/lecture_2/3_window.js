eb952158e7f4a09300bdc9c12a5298773ae5f564
Jasbir <mr.jasbirsingh96@gmail.com>
2023-11-04 22:28:45 +0530

    added explaination of window object

---
+++ Javascript/lecture_2/3_window.js
@@ -0,0 +1,29 @@
+/**
+ * EC : 
+ *      vars , fns
+ *      outer scope
+ *      window object
+ * 
+ * **/ 
+// hello=10;
+// console.log("hello",hello);
+// console.log("win",window.hello);
+
+// function fn(){
+//     console.log(window.hello);
+//     console.log(window);
+// }
+// fn();
+
+/*** fact-1
+ * JS -> only has the logic 
+ * Enviornment -> Provides the feature
+ * How does Enviornment help us to use these features -> Objects
+ * */ 
+
+/**fact-3
+ * Native object : that are part of JS -> Array, Date 
+ * Host Object: that are given to you by your env : window , document , console, localstorage 
+ * **/  
+console.log("Hello");
+obj={"name":"Jasbir"};
