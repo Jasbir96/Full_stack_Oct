5e4db139d1aa41956260466e1f03b116f4fb2eb6
Jasbir <mr.jasbirsingh96@gmail.com>
2023-12-01 06:57:54 +0530

    updated setup promises and microtask queue

---
+++ Javascript/Lecture_8_Promises_and_microtask_queue/cb_hell/inversion/client.js
@@ -0,0 +1,12 @@
+const runMlalgo = require("./lib");
+console.log("Before");
+let amount = 100;
+let priceofOne = 20;
+/**
+ * takes the amount and callbacks as paramas
+ * and it calls the cb for us
+ * */
+
+
+
+
