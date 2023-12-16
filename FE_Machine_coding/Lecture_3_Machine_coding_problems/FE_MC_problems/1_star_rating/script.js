c22f21c4e0144043cc852edc6dad13cc77b8f79c
Jasbir <mr.jasbirsingh96@gmail.com>
2023-12-16 22:00:39 +0530

    explained star component till click event

---
+++ FE_Machine_coding/Lecture_3_Machine_coding_problems/FE_MC_problems/1_star_rating/script.js
@@ -0,0 +1,70 @@
+// Must Have features
+//     * Rating from 1 to 5
+//         * when click -> rating should appear
+// : count should be printed
+// : stars should be highlighted
+//     * on hover -> highlight the stars
+//         * on mouse leave -> highlisghted stars should track back to 
+// previous state 
+
+// Good to have features -> optimization, accessibility
+//     * put emojis instead of numbers
+//         * slider based star rating
+// -->
+
+// 1. different point of interactions
+//     1. select elements -> that will change / cause changes
+//     2. add all the listeners
+//                 click 
+//                 mouse hover 
+//                 mouse leave 
+
+const starContainer = document.querySelector("#star_container");
+const countElement = document.querySelector("#count");
+const starArr = document.querySelectorAll(".star");
+
+starContainer.addEventListener("click", function (e) {
+    // console.log("click occured");
+    // console.log("target", e.target);
+    if (e.target == starContainer)
+        return;
+    // get the idx of your star
+    const cStar = e.target;
+    const idx = cStar.dataset.idx;
+    // update color till that idx 
+    fillcolors(idx);
+    countElement.textContent = idx;
+})
+
+
+function fillcolors(idx) {
+    // update color till that idx 
+    // reset the colors
+    for (let i = 0; i < starArr.length; i++) {
+        starArr[i].classList.remove("yellow");
+    }
+    //add the color
+    for (let i = 0; i < idx; i++) {
+        starArr[i].classList.add("yellow");
+    }
+}
+
+
+
+
+
+
+// starContainer.addEventListener("mouseover", function () {
+
+//     // console.log("hover occured");
+// })
+
+// starContainer.addEventListener("mouseleave", function () {
+//     console.log("leave occurred");
+// })
+
+
+
+
+
+
