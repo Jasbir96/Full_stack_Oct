fc897af255d66c66874a3344664dde0c94c4f932
Jasbir <mr.jasbirsingh96@gmail.com>
2024-01-17 08:29:18 +0530

    explained routing completely

--- React/Lecture_6/routing_context/src/poc/Routing.jsx
+++ React/Lecture_6/routing_context/src/poc/Routing.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {
     Route, Routes, Link,
-    Navigate
+    Navigate, useParams
 } from "react-router-dom";
 function Routing() {
     return (
@@ -21,7 +21,10 @@ function Routing() {
             <Routes>
                 <Route path="/home" element={<Home></Home>}></Route>
                 <Route path="/listing" element={<Listing></Listing>}></Route>
-                <Route path="/about" element={<About></About>}></Route>
+                <Route path="/about/*" element={<About></About>}></Route>
+                {/*  /users/*/}
+                {/* template routes -> dynamic routes  */}
+                <Route path="/user/:id" element={<User ></User>}> </Route>
                 <Route path="/" element={<Navigate to="/home"></Navigate>}></Route>
                 {/* 404 page not found -> wild card */}
                 <Route path="*" element={<PageNotFound></PageNotFound>}> </Route>
@@ -29,11 +32,26 @@ function Routing() {
         </>
     )
 }
-
 function About() {
     return (
-        <h2>About Page</h2>
+        <>
+            <h2>About Page</h2>
+            <Routes>
+                <Route path="/company"
+                    element={<Company />}> </Route>
+                <Route path="/founders"
+                    element={<Founder></Founder>}> </Route>
+            </Routes>
+        </>
     )
+
+
+}
+function Company() {
+    return <h4> We are  a good firm</h4>
+}
+function Founder() {
+    return <h4> We are Nice People </h4>
 }
 function Home() {
     return <h3>I am Home Page</h3>
@@ -41,6 +59,33 @@ function Home() {
 function Listing() {
     return <h3>I am Listing Page</h3>
 }
+
+function User(props) {
+    // while giving an object -> key as the the id and value as whatever we have passed
+    let params = useParams();
+    let [user, setUser] = useState(null);
+
+    // https://fakestoreapi.com/users/2
+    useEffect(() => {
+        (async function () {
+            const resp = await fetch(`https://fakestoreapi.com/users/${params.id}`)
+            const userData = await resp.json();
+            console.log(userData);
+            setUser(userData);
+        })()
+    }, [])
+    return <>
+        {user == null ? <h3>...loading</h3> : <>
+            <h4>User Name: {user.username}</h4>
+            <h3> Name: {user.name.firstname + " " + user.name.lastname}</h3>
+            <h4> Phone: {user.phone}</h4>
+        </>}
+    </>
+
+}
+
+
+
 function PageNotFound() {
     return <h3>Page Not found</h3>;
 }
@@ -50,9 +95,10 @@ export default Routing;
 /***
  * 1. redirect -> done
  * 2. Links work -> done 
- * 3. Template Route
- * 4. Nested ROutes 
- * 6. Programmatic redirects 
+ * 3. Template Route -> done 
+ * 4. Nested ROutes -> done
+ * 
+ * 6. Programmatic redirects -> done  useNavigate
  * 5. custom Routes (Auth )
  *          private Routes 
  *          conditionl Routes 
